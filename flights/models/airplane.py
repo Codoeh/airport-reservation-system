8bbbb1daeb5c3a2a8af7058d9203b5fd0ef7c84f
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-06-26 11:28:45 +0200

    add Airplane model

---
+++ flights/models/airplane.py
@@ -0,0 +1,20 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+from .airplane_type import AirplaneType
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.PositiveIntegerField()
+    seats_in_row = models.PositiveIntegerField()
+    airplane_type = models.ForeignKey(AirplaneType, on_delete=models.CASCADE)
+
+    def clean(self):
+        if self.rows <= 0 or self.seats_in_row <= 0:
+            raise ValidationError("Number of rows and seats in row must be higher than 0.")
+
+    @property
+    def total_seats(self):
+        return self.rows * self.seats_in_row
+
+    def __str__(self):
+        return f"{self.airplane_type}: {self.name}. Total seats: {self.total_seats}."
