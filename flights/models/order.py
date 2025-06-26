548357d964326963d4f91003882763ad316d4792
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-06-26 14:42:28 +0200

    add Order model

--- flights/models/order.py
+++ flights/models/order.py
@@ -1,5 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class Order(models.Model):
-    pass
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+
+    def __str__(self):
+        return f"Order #{self.id} by {self.user.username} at {self.created_at}"
+
+    class Meta:
+        ordering = ["-created_at"]
