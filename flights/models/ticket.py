e17bdfa954c7206fbd36e6c286c11e0bb7d98716
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-06-26 14:33:21 +0200

    add Ticket model

--- flights/models/ticket.py
+++ flights/models/ticket.py
@@ -1,5 +1,22 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+
+from flights.models import Airplane, Flight, Order
 
 
 class Ticket(models.Model):
-    pass
+    row = models.PositiveIntegerField()
+    seat = models.PositiveIntegerField()
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE, related_name="tickets")
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="tickets")
+
+    def __str__(self):
+        return f"Ticket {self.row}-{self.seat} on {self.flight}"
+
+    def clean(self):
+        airplane = self.flight.airplane
+        if self.row > airplane.rows or self.seat > airplane.seats_in_row:
+            raise ValidationError("Seat number is out of range for this airplane.")
+
+    class Meta:
+        unique_together = [["flight", "row", "seat"]]
