89b4a48417444bc94f2c726e16ee1f19bb8f5806
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-06-26 13:41:10 +0200

    add Route model

--- flights/models/route.py
+++ flights/models/route.py
@@ -1,5 +1,11 @@
 from django.db import models
+from .airport import Airport
 
 
 class Route(models.Model):
-    pass
+    source = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="sources")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="destinations")
+    distance = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.source}-{self.destination}"
