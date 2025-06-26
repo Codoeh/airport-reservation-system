73655c369900fef93c0aa22f82e63c879bf7523f
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-06-26 13:50:33 +0200

    add Crew model

--- flights/models/crew.py
+++ flights/models/crew.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+from flights.models import Flight
+
 
 class Crew(models.Model):
-    pass
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    flights = models.ManyToManyField(Flight)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
