a1ea6fc98105dd992d88afc607be963e04086fc5
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-06-26 14:18:39 +0200

    add Flight model

--- flights/models/flight.py
+++ flights/models/flight.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+from flights.models import Route, Airplane
+
 
 class Flight(models.Model):
-    pass
+    route = models.ForeignKey(Route, on_delete=models.CASCADE, related_name="flights")
+    airplane = models.ForeignKey(Airplane, on_delete=models.CASCADE, related_name="flights")
+    departure_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    arrival_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return (f"From - to: {self.route}. "
+                f"Plane: {self.airplane}. "
+                f"Departure time - arrival time: {self.departure_time} - {self.arrival_time}")
