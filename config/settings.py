ab5a36d2f362f72a5ccd97e058fe4a876297c821
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-07-05 19:58:26 +0200

    add DefaultPagination

--- config/settings.py
+++ config/settings.py
@@ -143,4 +143,6 @@ INTERNAL_IPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    "DEFAULT_PAGINATION_CLASS": "config.pagination.DefaultPagination",
+
 }
