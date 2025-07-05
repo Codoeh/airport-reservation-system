461da6b0a41a8dbc7e12e0bceedf7848c03c70c2
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-07-05 21:40:27 +0200

    add register view

--- user/views.py
+++ user/views.py
@@ -1,1 +1,10 @@
+from django.contrib.auth.models import User
+from rest_framework import generics
 
+from user.serializers import RegisterSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = []
