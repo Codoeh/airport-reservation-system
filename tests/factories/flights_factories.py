46fdb46acc1f333cd795f77478080f4bc222dd47
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-07-14 23:17:14 +0200

    Add AirplaneTypeFactory

--- tests/factories/flights_factories.py
+++ tests/factories/flights_factories.py
@@ -16,3 +16,10 @@ class AirportFactory(factory.django.DjangoModelFactory):
 
     name = factory.Sequence(lambda n: f"Airport{n}")
     closest_big_city = factory.Sequence(lambda n: f"City{n}")
+
+
+class AirplaneTypeFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = AirplaneType
+
+    name = factory.Sequence(lambda n: f"Type{n}")
